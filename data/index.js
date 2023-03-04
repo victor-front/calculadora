f3060a4827683b21f03fbf2ae67696299104b788
Victor Aguiar <victoraguiarrosa397@gmail.com>
2023-03-04 15:46:57 -0300

    a

--- data/index.js
+++ data/index.js
@@ -1,7 +1,22 @@
+let etapa = 0;
+let limite = 0;
+
 function load(){
 	for(c=0;c<=9;c++){
 		document.getElementById('teclado').innerHTML += `
 			<div class="botao" onclick="adicionar(${c})"><span>${c}</span></div>
 		`;
+	}
+}
+
+function adicionar(n){
+	if(etapa == 0){
+		if(limite >= 8){
+			window.alert('limite.');
+		}else{
+			limite++;
+			document.getElementById('saida').innerHTML += n;
+			console.log(Number(document.getElementById('saida')));
+		}
 	}
 }
