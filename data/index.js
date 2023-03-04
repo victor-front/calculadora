e992294280c5a6aea10785f45d0af101fce603e2
Victor Aguiar <victoraguiarrosa397@gmail.com>
2023-03-04 17:22:33 -0300

    resolvido

--- data/index.js
+++ data/index.js
@@ -1,5 +1,4 @@
 let etapa = 0;
-let limite = 0;
 
 function load(){
 	for(c=0;c<=9;c++){
@@ -9,14 +8,18 @@ function load(){
 	}
 }
 
-function adicionar(n){
+function adicionar(n, v, n2){
+	let said = document.getElementById('saida').innerHTML;
+	console.log(said);
+	
 	if(etapa == 0){
-		if(limite >= 8){
-			window.alert('limite.');
+		if(said.length >= 8){
+			n = Number(said);
+			etapa = 1;
+			console.log(n);
+			console.log(typeof n);
 		}else{
-			limite++;
 			document.getElementById('saida').innerHTML += n;
-			console.log(Number(document.getElementById('saida')));
 		}
 	}
 }
