389628547dd9675766bef8c5052c08146bc30538
Victor Aguiar <victoraguiarrosa397@gmail.com>
2023-03-05 15:57:07 -0300

    tentei

--- data/index.js
+++ data/index.js
@@ -1,25 +1,35 @@
 let etapa = 0;
 
 function load(){
-	for(c=0;c<=9;c++){
+	for(c=1;c<=9;c++){
 		document.getElementById('teclado').innerHTML += `
 			<div class="botao" onclick="adicionar(${c})"><span>${c}</span></div>
 		`;
 	}
+	document.getElementById('teclado').innerHTML += `
+		<div class="botao" onclick="adicionar(0)"><span>0</span></div>
+	`;
 }
 
-function adicionar(n, v, n2){
-	let said = document.getElementById('saida').innerHTML;
-	console.log(said);
-	
-	if(etapa == 0){
-		if(said.length >= 8){
-			n = Number(said);
-			etapa = 1;
-			console.log(n);
-			console.log(typeof n);
-		}else{
-			document.getElementById('saida').innerHTML += n;
+function adicionar(n){
+	if(n == "c"){
+		document.getElementById('saida').innerHTML = '';
+		etapa = 0;
+	}else{
+		let said = document.getElementById('saida').innerHTML;
+		if(etapa == 0){
+			if(said.length >= 8){
+				n1 = Number(said);
+				etapa = 1;
+			}else if(said.length <= 0){
+				document.getElementById('saida').innerHTML = n;
+				n1 = Number(said);
+				console.log(n1);
+			}else{
+				document.getElementById('saida').innerHTML += n;
+				n1 = Number(said);
+				console.log(n1);
+			}
 		}
 	}
 }
